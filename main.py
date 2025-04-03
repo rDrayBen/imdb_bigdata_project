911b2a36bbec8b1fa7a4a06a724e691db6c79b6e
tufimtseva <tatyana.ufimtseva@gmail.com>
2025-04-03 15:16:00 -0400

    cleared main.py

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-from data_preparation.title_akas_extract_transform import title_akas_extract_transform
-from business_queries.query_ufimtseva import compute_language_rating_trends
-from pyspark.sql import SparkSession
-
 def main():
-    spark = SparkSession.builder.appName(
-        "IMDb Dataset Processing").getOrCreate()
-    # dataset_path = "data/title.akas.tsv"
-    #
-    # df = title_akas_extract_transform(spark, dataset_path)
-    # df.printSchema()
-
-    title_basics = spark.read.csv("data/title_basics_cleaned.csv", sep="\t", #todo rename to tsv
-                                  header=True, inferSchema=True)
-    title_ratings = spark.read.csv("data/title.ratings.cleaned.tsv", sep="\t",
-                                   header=True, inferSchema=True)
-    title_akas = spark.read.csv("data/title.akas.cleaned.tsv", sep="\t", header=True,
-                                inferSchema=True)
-
-    df = compute_language_rating_trends(title_basics, title_ratings, title_akas)
-    df.show()
-
+    pass
 
 if __name__ == '__main__':
     main()
-    
