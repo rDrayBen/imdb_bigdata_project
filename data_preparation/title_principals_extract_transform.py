c8f1133daaba51c4f97b22904f9c037636aacfd6
RabotiahovDmytro <rabotiahovdmytriy@gmail.com>
2025-04-05 15:18:09 +0300

    Implemented business query and extract transform function

--- data_preparation/title_principals_extract_transform.py
+++ data_preparation/title_principals_extract_transform.py
@@ -1,12 +1,8 @@
-from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, lit, when
 from pyspark.sql.types import IntegerType, StringType, StructType, StructField
 
 
-def title_principals_extract_and_transform(data_path: str):
-    spark = SparkSession.builder.getOrCreate()
-
-    # Define schema explicitly
+def title_principals_extract_and_transform(spark, data_path: str):
     schema = StructType([
         StructField("tconst", StringType(), True),
         StructField("ordering", IntegerType(), True),
@@ -15,14 +11,8 @@ def title_principals_extract_and_transform(data_path: str):
         StructField("job", StringType(), True),
         StructField("characters", StringType(), True)
     ])
-
-    # Read the data with the defined schema
     df = spark.read.csv(data_path, sep="\t", header=True, schema=schema)
-
-    # Replace '\N' in 'characters' column with 'unknown'
     df = df.withColumn("characters", when(col("characters") == "\\N", "unknown").otherwise(col("characters")))
-
-    # Drop the 'job' column
     df = df.drop('job')
 
     return df
--- main.py
+++ main.py
@@ -1,35 +1,5 @@
-from pyspark.sql import SparkSession
-from business_queries.query_rabotiahov import top_genres_average_rating_over_decades
-
-
 def main():
-    # rabotiahov ===============================================================
-    spark = SparkSession.builder.appName("IMDB Analysis").getOrCreate()
-
-    # Read the title.basics DataFrame
-    title_basics_path = "/app/data/title_basics.csv"
-    title_basics_df = spark.read.csv(title_basics_path, sep="\t", header=True, inferSchema=True)
-
-    # Read the title.ratings DataFrame
-    title_ratings_path = "/app/data/title.ratings.cleaned.tsv"
-    title_ratings_df = spark.read.csv(title_ratings_path, sep="\t", header=True, inferSchema=True)
-
-    # Trim column names to remove leading/trailing spaces
-    title_basics_df = title_basics_df.select([col.strip() for col in title_basics_df.columns])
-    title_ratings_df = title_ratings_df.select([col.strip() for col in title_ratings_df.columns])
-
-    print("Title Basics Columns:", title_basics_df.columns)
-    print("Title Ratings Columns:", title_ratings_df.columns)
-    print(f"Total Rows: {title_basics_df.count()}")
-
-    # Call the function to compute the result
-    top_n = 5  # You can change this value as needed
-    result_df = top_genres_average_rating_over_decades(title_basics_df, title_ratings_df, top_n)
-
-    # Show the result in the console
-    result_df.show(truncate=False, n=1000)
-    # rabotiahov ===============================================================
-
+    pass
 
 if __name__ == '__main__':
     main()
