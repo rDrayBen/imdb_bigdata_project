2e770f20216a946df8be1576baed1461bc8a507b
Denys-Ratushniak <ratushniak.denis@gmail.com>
2025-04-06 15:03:04 +0300

    created extract transform pipeline for name_basics table, created business query count_actors_in_low_rated_popular_films

--- data_preparation/name_basics_extract_transform.py
+++ data_preparation/name_basics_extract_transform.py
@@ -2,9 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from pyspark.sql.types import StringType, StructType, StructField
 
-def name_basics_extract_transform_optimized(data_path: str):
-    spark = SparkSession.builder.getOrCreate()
-
+def name_basics_extract_transform(spark, data_path: str):
     schema = StructType([
         StructField("nconst", StringType(), True),
         StructField("primaryName", StringType(), True),
@@ -16,15 +14,16 @@ def name_basics_extract_transform_optimized(data_path: str):
 
     df = (
         spark.read.csv(data_path, sep="\t", header=True, schema=schema)
-        .replace("\\N", None)  # Replace "\N" with null values
-        .drop("birthYear", "deathYear")  # Drop unneeded columns
-        .dropna(subset=["primaryName"])  # Remove rows with null primaryName
-        .filter(col("primaryProfession").isNotNull() | col("knownForTitles").isNotNull())  # Remove rows where both are null
-        .na.fill("unknown")  # Replace remaining nulls with "unknown"
+        .replace("\\N", None)
+        .drop("birthYear", "deathYear")
+        .dropna(subset=["primaryName"]) 
+        .filter(col("primaryProfession").isNotNull() | col("knownForTitles").isNotNull())
+        .na.fill("unknown")
     )
 
     return df
 
 
 if __name__ == '__main__':
-    name_basics_extract_transform_optimized('/app/data/name.basics.tsv')
+    spark = SparkSession.builder.getOrCreate() 
+    name_basics_extract_transform(spark, '/app/data/name.basics.tsv')
