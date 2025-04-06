df03333a3b42e52a137cdd1e46e010b2bc6e8aa0
Denys-Ratushniak <ratushniak.denis@gmail.com>
2025-04-06 15:09:37 +0300

    used name baciscs extraction pipeline in main py, and added usage of count_actors_in_low_rated_popular_films business query

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+from pyspark.sql import SparkSession
+
+from data_preparation.name_basics_extract_transform import name_basics_extract_transform
+
+from business_queries.query_ratushniak import count_actors_in_low_rated_popular_films
+
 def main():
-    pass
+    spark = SparkSession.builder \
+        .appName("IMDB Data Analysis") \
+        .getOrCreate()
+    
+    # Load and transform data
+    name_basics_data_path = "data/name.basics.tsv"
+    name_basics_df = name_basics_extract_transform(spark, name_basics_data_path)
+
+    # Perform business queries
+    actors_in_low_rated_popular_films = count_actors_in_low_rated_popular_films(
+        title_ratings_df, 
+        title_principals_df, 
+        name_basics_df, 
+        average_rating=5.0, 
+        top_n=5000
+    )
+
+    print("Count actors in low rated popular films")
+    print(f"Total rows in resulting dataframe: {actors_in_low_rated_popular_films.count()}")
+    actors_in_low_rated_popular_films.show(truncate=False, n=20)
 
 if __name__ == '__main__':
-  main()
+    main()
